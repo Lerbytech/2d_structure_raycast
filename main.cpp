21cc75b14948d419db5fe0dade00fed8b2e1d1ee
Lerbytech <Lerbytech@users.noreply.github.com>
2018-05-19 16:23:09 +0300

    fill nodes and pretty print
    Added functions:
    1) fill node
    2) print them as list
    3) pretty print as grid

--- main.cpp
+++ main.cpp
@@ -75,9 +75,55 @@ Field read_config_file()
 	return RES;
 }
 
+Field prepare_field(Field f)
+{
+	int x = 0;
+	int y = 0;
+	int i = 0;
+	f.nodes = (Node*)malloc(sizeof(Node) * f.N);
+	for (y = 0; y < f.H; y++)
+		for (x = 0; x < f.W; x++)
+		{
+			f.nodes[i].val = i;
+			f.nodes[i].posX = x;
+			f.nodes[i].posY = y;
+			i++;
+		}
+	
+	return f;
+}
+
+void pretty_print(Field F)
+{
+	int x = 0;
+	int y = 0;
+
+	for (y = 0; y < F.H; y++)
+	{
+		for (x = 0; x < F.W; x++)
+		{
+			printf("%2d ", F.nodes[y * F.W + x].val);
+		}
+		printf("\n");
+	}
+
+}
+
+void line_by_line_print(Field F)
+{
+	for (int i = 0; i < F.N; i++)
+	{
+		printf("%2d %2d %2d\n", F.nodes[i].val, F.nodes[i].posX, F.nodes[i].posY);
+	}
+}
+
 int main()
 {
 	Field F;
 	F = read_config_file();
+
+	F = prepare_field(F);
+	line_by_line_print(F);
+	pretty_print(F);
 	int u = 4;
 }
